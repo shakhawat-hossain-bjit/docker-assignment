4781b0a36fd1239a5176c00448a9ff4eba9932fa
Shakhawat <shakhawat.hossain@bjitacademy.com>
2023-10-25 11:13:43 +0600

    database url for mongo

--- backend/db/config.js
+++ backend/db/config.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const databaseConnection = async (callback) => {
   try {
     console.log(
-      "process.env.DATABASE_URL ================> ",
-      process.env.DATABASE_URL
+      "process.env.DATABASE_URL_MONGO ================> ",
+      process.env.DATABASE_URL_MONGO
     );
-    if (process.env.DATABASE_URL) {
-      const client = await mongoose.connect(process.env.DATABASE_URL);
+    if (process.env.DATABASE_URL_MONGO) {
+      const client = await mongoose.connect(process.env.DATABASE_URL_MONGO);
       if (client) {
         console.log("Database connection successfully made");
         callback();
