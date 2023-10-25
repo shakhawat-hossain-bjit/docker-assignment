7c8b16ea71d0618c73d4915984a966f9f7964fb9
Shakhawat <shakhawat.hossain@bjitacademy.com>
2023-10-25 08:32:24 +0600

    database url

--- backend/db/config.js
+++ backend/db/config.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const databaseConnection = async (callback) => {
   try {
+    console.log(
+      "process.env.DATABASE_URL ================> ",
+      process.env.DATABASE_URL
+    );
     if (process.env.DATABASE_URL) {
       const client = await mongoose.connect(process.env.DATABASE_URL);
       if (client) {
